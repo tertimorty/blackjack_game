c49be8b686b9ad4d6f530ca6054cfc7c4408404f
Kaspars Blumbergs <kaspar.blumberg@gmail.com>
2019-02-24 21:38:51 +0200

    gala versija

--- My_first_piton_script.py
+++ My_first_piton_script.py
@@ -6,14 +6,57 @@ this document will contain functions for gameEngine, that are not game engine it
 '''
 
 
+def take_bet(chips):
+
+    while True:
+        try:
+            chips.bet = int(input('How many chips would you like to bet? '))
+        except ValueError:
+            print('Sorry, a bet must be an integer!')
+        else:
+            if chips.bet > chips.total:
+                print("Sorry, your bet can't exceed",chips.total)
+            else:
+                break
+
 def hit(deck,hand):
-    
     hand.add_card(deck.deal())
     hand.adjust_for_ace()
+    
+def hit_or_stand(deck,hand):
+    global playing
+    
+    while True:
+        x = input("Would you like to Hit or Stand? Enter 'h' or 's' ")
+        
+        if x[0].lower() == 'h':
+            hit(deck,hand)  # hit() function defined above
+            playing = True
+        elif x[0].lower() == 's':
+            print("Player stands. Dealer is playing.")
+            playing = False
 
+        else:
+            print("Sorry, please try again.")
+            continue
+        return playing
+        break
 
-
-
+    
+def show_some(player,dealer):
+    print("\nDealer's Hand:")
+    print(" <card hidden>")
+    print('',dealer.cards[1])  
+    print("\nPlayer's Hand:" + str(player.value), *player.cards, sep='\n ')
+    print(___________________________________________________________________)
+    
+def show_all(player,dealer):
+    print("\nDealer's Hand:", *dealer.cards, sep='\n ')
+    print("Dealer's Hand =",dealer.value)
+    print("\nPlayer's Hand:", *player.cards, sep='\n ')
+    print("Player's Hand =",player.value)
+    print(___________________________________________________________________)
+    
 def player_busts(player,dealer,chips):
     print("Player busts!")
     chips.lose_bet()
